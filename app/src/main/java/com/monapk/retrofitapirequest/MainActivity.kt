f032bf2e545fab49d37c29200b458852709565b9
mona-apk <cordion_nap@yahoo.co.jp>
2020-12-18 15:50:42 +0900

    feat: APIリクエスト

--- app/src/main/java/com/monapk/retrofitapirequest/MainActivity.kt
+++ app/src/main/java/com/monapk/retrofitapirequest/MainActivity.kt
@@ -2,10 +2,24 @@ package com.monapk.retrofitapirequest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.monapk.retrofitapirequest.network.request.GurunaviApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val response = GurunaviApiService().getRestaurant()
+            response?.run {
+                this.rest.forEach {
+                    Log.d("tag", it.toString())
+                }
+            }
+        }
     }
 }
